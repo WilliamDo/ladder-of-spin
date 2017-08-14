0982dc01ce21f8ee2587edf80bfcec1107444f01
William Do <william@ultimaspin.com>
2017-08-14 18:55:14 +0100

    call remote service for ladder details

--- src/top_ladder/core.cljs
+++ src/top_ladder/core.cljs
@@ -1,41 +1,61 @@
 (ns top-ladder.core
-    (:require
-      [reagent.core :as r]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require
+    [cljs.reader]
+    [reagent.core :as r]
+    [cljs-http.client :as http]
+    [cljs.core.async :refer [<!]]))
 
-(def player-self (r/atom {:position 6}))
+(def player-store (r/atom {}))
 
-(def names '["William" "Kaho" "Michael" "Tom" "Trevor" "Clive" "Colin"])
+(def player-self (r/atom {:id 3}))
 
 (defn challenge-player [id name]
   (let [confirm-challenge (js/confirm (str "Do you want to challenge " name "?"))]
     (if confirm-challenge
-      (swap! player-self #(assoc-in % [:challenge] id)))))
+      (go (let [response (<! (http/post "http://localhost:8080/challenge" {:form-params {:challenger (:id @player-self) :challengee id}}))]
+          (prn response)
+          (prn (:body response))
+          (swap! player-self #(assoc-in % [:challenge :opponent] id)))))))
 
 (defn can-challenge? [my-pos other-pos] (< 0 (- my-pos other-pos) 5))
 
-(def player-store
-  (r/atom
-    (map
-      (fn [name id]
-        (let [pos (- (count names) id)] {:name name :id id :position pos}))
-      names
-      (range (count names)))))
+(defn clear-challenge [] (swap! player-self #(dissoc % :challenge)))
+
+(defn challenge-win [id]
+  (go (let [response (<! (http/post "http://localhost:8080/confirm-result" {:form-params {:player (:id @player-self) :opponent id :result :win}}))]
+      (reset! player-store (get-in response [:body :players]))
+      (prn (get-in response [:body :challenges (:id @player-self)]))
+      (swap! player-self assoc :challenge (get-in response [:body :challenges (:id @player-self)])))))
+
+(defn challenge-loss [id]
+  (go (let [response (<! (http/post "http://localhost:8080/confirm-result" {:form-params {:player (:id @player-self) :opponent id :result :loss}}))]
+      (reset! player-store (get-in response [:body :players]))
+      (prn (get-in response [:body :challenges (:id @player-self)]))
+      (swap! player-self assoc :challenge (get-in response [:body :challenges (:id @player-self)])))))
+
+(defn refresh []
+  (go (let [response (<! (http/get "http://localhost:8080/ladder"))]
+      (prn response)
+      (prn (:body response))
+      (reset! player-store (get-in response [:body :players]))
+      (swap! player-self assoc :challenge (get-in response [:body :challenges (:id @player-self)])))))
 
 ;; -------------------------
 ;; Views
 
 (defn challenge-list [players]
-  [:ul
+  [:ol
    (for [p players]
      ^{:key (:id p)}
      [:li
       (:name p)
-      (if (can-challenge? (:position @player-self) (:position p))
+      (if (can-challenge? (:position (get @player-store (:id @player-self))) (:position p))
         [:button {:on-click #(challenge-player (:id p) (:name p))} "Challenge"]
         "")])])
 
 (defn ladder-list [players]
-  [:ul
+  [:ol
    (for [p players]
      ^{:key (:id p)} [:li (:name p)])])
 
@@ -43,16 +63,24 @@
   [:div
    "Players"
    (if (:challenge @player-self)
-     [ladder-list (sort-by :position @player-store)]
-     [challenge-list (sort-by :position @player-store)])])
+     [ladder-list (sort-by :position (vals @player-store))]
+     [challenge-list (sort-by :position (vals @player-store))])])
 
-(defn challenge-info [] [:div "You are currently engaged in a challenge with player " (:challenge @player-self)])
+(defn challenge-info []
+  (let [challenge-opponent (get-in @player-self [:challenge :opponent]) claim (get-in @player-self [:challenge :result])]
+  [:div
+   "You have a pending challenge with "
+   (:name (get @player-store challenge-opponent))
+   [:div
+    [:button {:on-click #(challenge-win challenge-opponent)} "Win"]
+    [:button {:on-click #(challenge-loss challenge-opponent)} "Lose"]]
+   [:div "Your claim is " claim]]))
 
 (defn challenge-display []
   (if (:challenge @player-self) [challenge-info]))
 
 (defn home-page []
-  [:div [:h2 "Top Ladder"] [ladder-display] [challenge-display]])
+  [:div [:h2 "Top Ladder"] [ladder-display] [challenge-display] [:button {:on-click #(refresh)} "Refresh"]])
 
 ;; -------------------------
 ;; Initialize app
