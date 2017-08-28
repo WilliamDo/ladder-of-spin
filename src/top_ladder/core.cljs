190ed9d36b8d045f4c777da9525df792025b94f0
William Do <william@ultimaspin.com>
2017-08-28 17:29:51 +0100

    use bootstrap for ui

--- src/top_ladder/core.cljs
+++ src/top_ladder/core.cljs
@@ -8,7 +8,7 @@
 
 (def player-store (r/atom {}))
 
-(def player-self (r/atom {:id 3}))
+(def player-self (r/atom {:id 4}))
 
 (defn challenge-player [id name]
   (let [confirm-challenge (js/confirm (str "Do you want to challenge " name "?"))]
@@ -55,32 +55,53 @@
         "")])])
 
 (defn ladder-list [players]
-  [:ol
-   (for [p players]
-     ^{:key (:id p)} [:li (:name p)])])
+  (let [current-player @player-self]
+    [:table {:class "table table-hover"}
+     [:thead {:class "thead-inverse"} [:tr [:th "#"] [:th "Name"] [:th "Challenge"]]]
+     [:tbody (for [p players]
+               [:tr {:class (if (= (:id p) (:id @player-self)) "table-info")}
+                [:th {:scope "row"} (:position p)]
+                [:td (:name p)]
+                [:td (if (and (nil? (:challenge current-player)) (can-challenge? (:position (get @player-store (:id current-player))) (:position p)))
+                  [:button {:on-click #(challenge-player (:id p) (:name p)) :class "btn btn-outline-primary btn-sm"} "Challenge"])]])]]))
 
 (defn ladder-display []
-  [:div
-   "Players"
-   (if (:challenge @player-self)
-     [ladder-list (sort-by :position (vals @player-store))]
-     [challenge-list (sort-by :position (vals @player-store))])])
+  [:div [ladder-list (sort-by :position (vals @player-store))]])
 
 (defn challenge-info []
-  (let [challenge-opponent (get-in @player-self [:challenge :opponent]) claim (get-in @player-self [:challenge :result])]
+  (let [challenge-opponent (get-in @player-self [:challenge :opponent])
+        claim (get-in @player-self [:challenge :result])]
   [:div
    "You have a pending challenge with "
    (:name (get @player-store challenge-opponent))
    [:div
-    [:button {:on-click #(challenge-win challenge-opponent)} "Win"]
-    [:button {:on-click #(challenge-loss challenge-opponent)} "Lose"]]
-   [:div "Your claim is " claim]]))
+    [:button {:on-click #(challenge-win challenge-opponent) :class "btn btn-outline-success btn-sm"} "Win"]
+    [:button {:on-click #(challenge-loss challenge-opponent) :class "btn btn-outline-secondary btn-sm"} "Lose"]]
+   [:div (if claim (str "You are claiming " claim) "You have not entered a result")]]))
 
 (defn challenge-display []
   (if (:challenge @player-self) [challenge-info]))
 
+(defn challenge-modal []
+  (let [challenge-opponent (get-in @player-self [:challenge :opponent])
+        claim (get-in @player-self [:challenge :result])]
+    [:div {:class "modal fade" :id "challengeModal" :tabIndex "-1" :role "dialog" :aria-labelledby "challengeModalLabel" :aria-hidden "true"}
+     [:div {:class "modal-dialog" :role "document"}
+      [:div {:class "modal-content"}
+       [:div {:class "modal-header"} "Challenge"]
+       [:div {:class "modal-body"} "You have a pending challenge with " (:name (get @player-store challenge-opponent)) [:div (if claim (str "You are claiming " claim) "You have not entered a result")]]
+       [:div {:class "modal-footer"}
+        [:button {:on-click #(challenge-win challenge-opponent) :class "btn btn-outline-success btn-sm"} "Win"]
+        [:button {:on-click #(challenge-loss challenge-opponent) :class "btn btn-outline-secondary btn-sm"} "Lose"]]]]]))
+
 (defn home-page []
-  [:div [:h2 "Top Ladder"] [ladder-display] [challenge-display] [:button {:on-click #(refresh)} "Refresh"]])
+  [:div
+   [:nav {:class "navbar navbar-dark bg-primary"}
+    [:span {:class "navbar-brand mb-0"} "Top Ladder"]
+    (if (:challenge @player-self) [:button {:class "btn btn-primary btn-sm" :data-toggle "modal" :data-target "#challengeModal"} "Challenge"])
+    [:button {:on-click #(refresh) :class "btn btn-primary btn-sm"} "Refresh"]]
+
+   [:div {:class "container"} [ladder-display] [challenge-modal]]])
 
 ;; -------------------------
 ;; Initialize app
